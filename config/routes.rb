0f7781b15a457b894dfd1fbb4f60894791a804b1
Patrick Brennan <pbrennanmusic@gmail.com>
2021-09-04 17:31:57 -0400

    Created controller files and added resources for each controller to config/routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
+  resources :music_directors, only: [:index, :show, :edit, :update, :destroy]
+  resources :musicians, only: [:index, :show, :edit, :update, :destroy]
+  resources :gigs
+  resources :instruments, only: [:index, :new, :create, :show]
+  resources :genres, only: [:index, :new, :create, :show]
 end
